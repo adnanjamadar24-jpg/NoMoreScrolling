aae30c2acc127dd0784a94568c59cfd3afcc2412
adnanjamadar24-jpg <adnanjamadar24@gmail.com>
2025-11-21 12:46:44 +0530

    Add files via upload

---
+++ BlockScreen.java
@@ -0,0 +1,12 @@
+package com.example.nomorescrolling;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlockScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.block_screen);
+    }
+}
