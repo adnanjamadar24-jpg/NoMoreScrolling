c849cc28da3602481f17890676b51d363e674581
adnanjamadar24-jpg <adnanjamadar24@gmail.com>
2025-12-24 10:57:09 +0530

    Delete AppInfo.java

--- AppInfo.java
+++
@@ -1,8 +0,0 @@
-package com.example.nomorescrolling;
-
-import android.graphics.drawable.Drawable;
-
-public class AppInfo {
-    public String name; public String packageName; public int limitMinutes; public boolean blockMode; public Drawable icon;
-    public AppInfo(String name, String pkg, int limitMinutes, boolean blockMode){ this.name=name; this.packageName=pkg; this.limitMinutes=limitMinutes; this.blockMode=blockMode;}
-}
